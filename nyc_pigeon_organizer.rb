13900547663ef17e01aabac47ccbca21f6acfd9e
Chovin Carlson <Chovin@users.noreply.github.com>
2018-09-14 06:09:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  ret = {}
+  data.each {|attr, value_hash|
+    attr
+    value_hash.each {|value, name_list|
+      value
+      name_list.each {|name|
+        pige = (ret[name] ||= {})
+        pige[attr] ||= []
+        pige[attr] << value.to_s
+      }
+    }
+  }
+  ret
 end
