4395a54c3c49162a7a90e33af12e6a4c3f6b6fb8
Matthew McCormack <mmccormack1182@gmail.com>
2018-10-05 09:16:00 -0400

    Tests Passed

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data[:gender].each do |category, names|
+    names.each do |name|
+      new_hash[name] = {}
+    end
+  end
+  data.each do|attribute, values|
+    values.each do |category, names|
+      names.each do |name|
+        if new_hash[name].has_key? (attribute)
+          new_hash[name][attribute].push (category.to_s)
+        elsif new_hash.has_key?(name)
+          new_hash[name][attribute] = [category.to_s]
+        end
+      end
+    end
+  end
+  new_hash
 end
