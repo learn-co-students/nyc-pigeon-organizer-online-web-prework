30a12278c4cb101eda9b15c5ef3693adbee45756
Brandon Teich <brandonteich123@gmail.com>
2018-10-16 04:08:54 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,3 +17,16 @@ list
 end 
 end 
 end
+ data.each {|attr, value_hash|
+    attr
+    value_hash.each {|value, name_list|
+      value
+      name_list.each {|name|
+        pige = (ret[name] ||= {})
+        pige[attr] ||= []
+        pige[attr] << value.to_s
+      }
+    }
+  }
+  ret
+end
