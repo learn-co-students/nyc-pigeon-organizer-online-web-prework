f2b14774498e9185300405cbcc4a97ebb09393b3
Paul Ashour <paul.ashour@gmail.com>
2018-10-20 01:06:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,9 +6,15 @@ def nyc_pigeon_organizer(data)
   data.each do |cat, att_hash|
     att_hash.each do |att, name_list|
       name_list.each do |name|
+<<<<<<< HEAD
         pigeons_by_name[name] ||= {}
         pigeons_by_name[name][cat] ||= []
         pigeons_by_name[name][cat].push att.to_s
+=======
+        binding.pry
+        pigeons_by_name[name][cat] ||= []
+        pigeons_by_name[name][cat].push att
+>>>>>>> 501dab2d1f8d939d1883a7216379cd9ddff114ea
       end
     end
   end
