2acfc562038bd71637bf5581f5153d1bdff656cc
VICTORIA MANUEL <victoriamanuel.dev@gmail.com>
2018-10-21 19:09:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+data[:gender].map do |category, names|
+    names.map do |name|
+      pigeon_list[name] = {}
+    end
+  end
+  
+  data.map do |attribute, hash|
+    hash.map do |category, names|
+      names.map do |name|
+        if pigeon_list[name].include?(attribute)
+          pigeon_list[name][attribute].push(category.to_s)
+        elsif pigeon_list.include?(name)
+          pigeon_list[name][attribute] = [category.to_s]
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
+ 
+
