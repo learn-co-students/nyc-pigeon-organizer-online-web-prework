d0d32a25d8409b7a2571ac2f187877794cff123c
Dmitri Samotessov <dimasamo96@gmail.com>
2018-11-12 20:13:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  ret_hash = {}
+  data.each do |attributes_keys, attribute_hash|
+    attribute_hash.each do |specifics, name_array|
+      name_array.each do |name|
+        if !ret_hash.has_key?(name)
+          ret_hash[name] = {attributes_keys => [specifics.to_s]}
+        ##  binding.pry
+        elsif !ret_hash[name].has_key?(attributes_keys)
+          ret_hash[name][attributes_keys]=[specifics.to_s]
+        else
+          ret_hash[name][attributes_keys].push(specifics.to_s)
+        end
+      end
+    end
+  end
+  ret_hash
 end
