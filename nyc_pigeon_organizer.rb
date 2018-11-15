367dd590d467aba9a5361f8e178d14e37e392313
Yuki N <lunarbase@outlook.com>
2018-11-15 05:20:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  obj = Hash.new{|h,k| h[k] = {color: [], gender: [],lives: []}}
+
+  data.each_with_object(obj) do |(key, value), obj|
+    value.each do |item,array|
+      array.each do |string| 
+      
+        obj[string][:color] << item.to_s if key == :color
+        obj[string][:gender] << item.to_s if key == :gender
+        obj[string][:lives] << item.to_s if key == :lives
+
+      end
+    end
+  end
 end
