4e481d54f7b2ae17736b182ccd34a54538724fdb
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 05:13:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |attribute, values|
+    values.each do |value, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon] ||= {}
+        pigeon_list[pigeon][attribute] ||= []
+        pigeon_list[pigeon][attribute] << value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
