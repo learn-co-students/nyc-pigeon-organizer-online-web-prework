d827c4d4fccac6ec3a798e45a5607824c148aef7
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 21:17:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
+require 'pry'
+pigeon_list={}
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each do  |pigeon_attribute,key|
+    key.each do   |key, names|
+      birds_names=names.join(" ")
+pigeon_list[birds_names]= pigeon_attribute
+binding.pry
+      end
+    end
+  return update_hash
 end
