5f7b4c43eea49c77724460f5287b731e5a3585cc
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-04 00:59:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,15 +5,15 @@ def nyc_pigeon_organizer(data)
   data.each do  |pigeon_attribute,key|
     key.each do   |key, names|
       names.each  do |bird_name|
-        if pigeon_list[bird_name] == nil
-        pigeon_list[bird_name]= {pigeon_attribute=> key.to_s}
+         pigeon_list[bird_name] ||= {}
+         pigeon_list[bird_name][pigeon_attribute] ||= []
+          #  binding.pry
 
-        else
-          pigeon_list[bird_name][pigeon_attribute]= [key.to_s]
-        end
+          pigeon_list[bird_name][pigeon_attribute] << key.to_s
+
         #binding.pry
       end
     end
   end
-  #return update_hash
+  return pigeon_list
 end
