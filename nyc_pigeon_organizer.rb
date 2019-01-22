6b5651e0c70e637f6bf6af7ed14f7f572b29be86
Vinh Do <vinhdo07@gmail.com>
2019-01-22 20:37:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,6 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_org = {}
   pigeon_ary = []
+
   data.each_value do |stats|
     pigeon_ary << stats.values
   end
@@ -12,6 +13,17 @@ def nyc_pigeon_organizer(data)
     pigeon_org[pigeon_name] = {}
   end
 
-  binding.pry
 
+    data.each do |key, stats|
+      stats.each do |characteristic, name|
+        name.each do |pigeon_name|
+          if pigeon_org[pigeon_name].has_key?(key)
+            pigeon_org[pigeon_name][key] << characteristic.to_s
+          else
+            pigeon_org[pigeon_name][key] = [characteristic.to_s]
+          end
+        end
+      end
+    end
+  pigeon_org
 end
