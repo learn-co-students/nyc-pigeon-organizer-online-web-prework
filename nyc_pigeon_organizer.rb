d8a72d087a5bfe02fade0ec83043cac24f726acf
Vinh Do <vinhdo07@gmail.com>
2019-01-22 05:17:33 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_org = {}
+  pigeon_ary = []
+  data.each_value do |stats|
+    pigeon_ary << stats.values
+  end
+  pigeon_ary = pigeon_ary.flatten.uniq
+
+  pigeon_ary.each do |pigeon_name|
+    pigeon_org[pigeon_name] = {}
+  end
+
+  binding.pry
+
 end
