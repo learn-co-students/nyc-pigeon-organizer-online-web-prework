62e909d870c83a3006bdbb4ac7c19f9c8e29b6aa
Mark Kanko <mkanko66@gmail.com>
2019-02-18 02:28:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,16 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  pigeon_data[:gender][:male].each do |item|
-binding.pry
+  
+  data.each do |attribute_key, value_hash|
+    
+    if attribute_key == [:gender]
+    
+      value_hash.each do |gender_key, name_array|
+        
+#binding.pry
+
+      end    
+    end 
   end
 end
