919cee26e27f709fc25c09fe3cb410798d2de2d6
Mark Kanko <mkanko66@gmail.com>
2019-02-19 00:50:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,18 +4,36 @@ def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
   
+  all_names = []
+  
   data[:gender].each do |gender_key, name_array|
     
-#    name_array.each do |name|
+    name_array.each do |name|
       
-      pigeon_list = Hash[name_array.collect {|name| [name, ""]}]
-        
-        
+      all_names << name 
       
-#binding.pry
-
-#     end 
+      pigeon_list = Hash[all_names.collect {|name| [name, {}]}]
+      
+      data.each do |a_key, attribute_hash|
+        
+        attribute_hash.each do |attribute_key, b_value|
+        
+      pigeon_list.each do |name_key, value|
+        
+        if name_key == data[a_key][attribute_key][name]
+          
+          
+        
+             
+        end   
+         
+      end  
+      
+    end
+    
   end
+  pigeon_list  
+  binding.pry 
 end
 
 
@@ -27,7 +45,26 @@ end
 # names exist in :male [] and :female [] and are not 
 # repeated.
 
+# Added a step above. Combined (<< each into new [] - lines
+# 11 & 13) all pigeons in :male and :female arrays into new
+#  all_names [] in order to apply line 15 to all pigeons.
+
 # 3. Each pigeon name which is an element in :male []  and  
 # :female [] needs to be collected and made a key in pigeon
 # hash. Can this be done without assigning a value to the key?
 # Can I instantiate a hash with just keys in it? 
+
+# 3. YES, it is "instantiating" a new hash, notice the capitalized
+# H in "hash" referring to "class" in the line of code below
+# which is "creating" (instantiating) a new "instance" of the 
+# class - Hash, also the line of code below instaniates a new 
+# hash and collects the elements of an array converting them 
+# into keys in the new hash, not existing by themselves, but 
+# as key pointing at an empty value, so the elements are 
+# turned into a key value pair, but a value that is empty to 
+# be added to later.
+
+# from line 15 above: 
+# pigeon_list = Hash[all_names.collect {|name| [name, ""]}]
+
+# 4. Now 
