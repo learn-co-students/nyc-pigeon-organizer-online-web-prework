baaf7ab89b3d30508c6ab7a77f5773a3d3f7e405
cavu757 <jose.pehovazdiez@gmail.com>
2019-02-20 20:19:43 -0500

    Complete

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  t_hash = {}
+
+  data.each do |attribute, value|
+    value.each do |value, names|
+      names.each do |name|
+        t_hash[name] ||= {}
+        t_hash[name][attribute] ||= []
+        t_hash[name][attribute] << value.to_s
+      end
+    end
+  end
+  #binding.pry
+  t_hash
 end
