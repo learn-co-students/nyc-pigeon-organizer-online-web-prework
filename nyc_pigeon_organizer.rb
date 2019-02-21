fb789a09211972bf58a6ddd3768fa7a3ac428019
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-02-21 01:20:50 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
+<<<<<<< HEAD
   t_hash = {}
 
   data.each do |attribute, value|
@@ -15,3 +16,24 @@ def nyc_pigeon_organizer(data)
   #binding.pry
   t_hash
 end
+=======
+  arr1 = []
+  arr2 = []
+  arr3 = []
+  hsh1 = {}
+  hsh2 = {}
+  hsh3 = {}
+  a_ct = 0 
+  b_ct = 0 
+  c_ct = 0
+  
+  data.each do |attribute, value|
+    value.each do |group, name|
+      #arr1.concat(name) 
+      
+    end
+  end
+  #arr1 = arr1.uniq!
+  
+end
+>>>>>>> c4fcd7134c9b8f4d779911f5ad064731619fd92d
