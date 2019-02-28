a1db0ad5975d7f7d36de0620a17cabdc6ae8a55d
Junyoung McArdle <juny.mcardle@gmail.com>
2019-02-28 21:23:00 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # get the name as first key of new_hash
+  # add the first key from data to new_hash as a key in value_hash
+  # add the key of value_hash from data to new_hash as a value in the value_hash
+  new_hash ={}
+  data.each do |key, value_hash|
+    value_hash.each do |tags, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][key] ||= []
+        new_hash[name][key] << tags.to_s
+      end
+    end
+  end
+  new_hash
 end
