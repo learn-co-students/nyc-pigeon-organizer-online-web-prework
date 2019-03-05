f201de5be01f5d2d554ab18f24181f728bb51f27
Cody Yantis <cyantis@gmail.com>
2019-03-05 01:45:25 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  data.each do |trait, quality|
+    quality.each do |aspect, name|
+      name.each do |pigeon_name|
+        pigeon_hash[pigeon_name] ||= pigeon_hash[pigeon_name] = {}
+        pigeon_hash[pigeon_name][trait] ||= pigeon_hash[pigeon_name][trait] = []
+        pigeon_hash[pigeon_name][trait] << aspect.to_s
+      end
+    end
+  end
+  pigeon_hash
 end
