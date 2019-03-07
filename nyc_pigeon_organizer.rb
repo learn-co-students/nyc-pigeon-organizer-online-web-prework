546e58c9a7f9883d90747285e56c18c3677cee0b
Irene Saez <bluemilk44@gmail.com>
2019-03-07 22:27:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, name|
+    name.each do |name|
+      result[name] ||= {}
+      result[name][key] ||= []
+      result[name][key].push(value.to_s)
+    end
+  end
+  end
 end
