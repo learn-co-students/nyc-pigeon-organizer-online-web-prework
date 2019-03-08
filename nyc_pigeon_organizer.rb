8f3429c782524f9ce7c233c2b0b120c5f5b5c9b5
Eyal Toledano <eyaltoledano@me.com>
2019-03-08 14:49:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,36 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+require 'pry'
+
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list = {}
+  pigeon_data.each do |attribute, trait|
+    trait.each do |data, name|
+      name.each do |pigeon|
+        if !pigeon_list.keys.include?(pigeon)
+          pigeon_list[pigeon] = {}
+        end
+      end
+    end
+  end
+
+  pigeon_data.each do |attribute, trait|
+    pigeon_list.each do |pigeon, data|
+      data[attribute] = [] if attribute == :color
+      data[attribute] = [] if attribute == :gender
+      data[attribute] = [] if attribute == :lives
+    end
+  end
+
+  pigeon_data.each do |attribute, trait|
+    binding.pry
+    trait.each do |data, name|
+      pigeon_list.each do |pigeon, pigeon_data|
+        pigeon_data.each do |new_trait, new_data|
+          
+
+        end
+      end
+    end
+  end
+
+  pigeon_list
 end
