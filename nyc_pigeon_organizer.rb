b3214facbf1d423f10179b06e929f779a269887f
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-10 01:27:06 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,5 +1,11 @@
 def nyc_pigeon_organizer(data)
-  data.map do |color, name|
-    
-  # write your code here!
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
 end
