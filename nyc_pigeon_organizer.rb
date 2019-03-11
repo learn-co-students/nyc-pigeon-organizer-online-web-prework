4b538bff7b2a4c0d5900b1cb8aca878503c65b86
Natasha Chernis <natashachernis@gmail.com>
2019-03-11 06:26:19 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  pigeon_names = data.map {|category, attributes|
+    attributes.map {|attribute, pigeons| pigeons}
+  }.flatten.uniq!
+
+  pigeon_list = {}
+  pigeon_names.each {|pigeon| pigeon_list[pigeon] = {}
+  }
+
+  pigeon_list.each {|pigeon, pigeon_data|
+    data.map {|category, attributes|
+      attributes.map {|attribute, pigeons|
+        if pigeons.include?(pigeon)
+          if pigeon_list[pigeon][category].class == Array
+            pigeon_list[pigeon][category] << attribute.to_s
+          else
+           pigeon_list[pigeon][category] = [attribute.to_s]
+          end
+        end
+      }
+    }
+  }
+
 end
