07cc534d21bbed1c56bab9a94d143098534a1b74
Ethan <ethansagin@gmail.com>
2019-03-27 15:48:07 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer(hsh)
+  pigeon_list = {}
+  hsh.each { |trait, data|
+    data.each { |k, v|
+      v.each { |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][trait] ||= []
+        pigeon_list[name][trait] << k.to_s
+      }
+    }
+  }
+  pigeon_list
 end
+
