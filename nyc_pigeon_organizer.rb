bcd09da5b80f86699d56e2463c13d1d41983786e
Caleb Einarr <earthctzn1@gmail.com>
2019-04-14 16:51:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  data.each_with_object({}) do |(key, x), new_hash|
+    x.each do |value, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][key] ||= []
+        new_hash[name][key] << value.to_s
+      end
+    end
+  end
 end
