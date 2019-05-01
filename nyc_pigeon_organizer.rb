6599965862d38874a196b116a93fede31e12d371
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-01 01:29:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  bitches = {}
+  data.each do |keys, color_data|
+    color_data.each do |ind_keys, pigeons_chunk|
+      pigeons_chunk.each do |pigeon|
+        bitches[pigeon] = {}
+        bitches[pigeon] = keys
+        
+        binding.pry
+        
+      end
+    end
+  end
+  bitches
+ #binding.pry
 end
