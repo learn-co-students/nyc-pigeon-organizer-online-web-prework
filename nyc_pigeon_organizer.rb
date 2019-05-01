c74bdaeb79d729893066f5083eb6a5f99f928e7d
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-01 04:19:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,19 +2,18 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   bitches = {}
   data.each do |keys, color_data|
-    
     color_data.each do |ind_keys, ind_values|
-      
       ind_values.each do |pigeon|
-        if !bitches[pigeon]
+        
+        #if !bitches[pigeon]
           bitches[pigeon] = {}
-          if !bitches[pigeon][keys]
-            bitches[pigeon][keys] = []
-            if !bitches[pigeon][keys].include? ind_keys.to_s
-              bitches[pigeon][keys] << ind_keys.to_s
-          
-          
-            end
+        
+        if !bitches[pigeon][keys]
+          bitches[pigeon][keys] = []
+            
+         if !bitches[pigeon][keys].include? ind_keys.to_s
+           bitches[pigeon][keys] << ind_keys.to_s
+            
           end
         end
       end
