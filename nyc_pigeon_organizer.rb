19192f3b4136649fae0374d5e90caa9711a4eddf
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-01 02:03:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,16 +2,22 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   bitches = {}
   data.each do |keys, color_data|
-    color_data.each do |ind_keys, pigeons_chunk|
-      pigeons_chunk.each do |pigeon|
-        bitches[pigeon] = {}
-        bitches[pigeon] = keys
+    color_data.each do |ind_keys, ind_values|
+      binding.pry
+      ind_values.each do |pigeon|
+        bitches[pigeon] = []
+        
+         if color_data.value? pigeon
+           bitches[pigeon] << color_data
+        
+        
         
         binding.pry
         
+        end
       end
     end
   end
   bitches
- #binding.pry
+ binding.pry
 end
