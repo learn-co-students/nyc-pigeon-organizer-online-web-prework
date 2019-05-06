568481bf054a15bd6da48f19ee43cac19d4a51ec
Brian Velez <bvez94@gmail.com>
2019-05-05 18:01:45 -0700

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+
+  data.each do |symbol, values|
+    values.each do |attribute, names|
+      names.each do |element|
+        if pigeon_list[element] == nil #if Pigeon not in list
+          pigeon_list[element] = {symbol => [attribute.to_s]} #create the hash for the pigeon
+          #binding.pry 
+
+        elsif pigeon_list[element][symbol]
+          pigeon_list[element][symbol] << attribute.to_s
+
+        else
+          temp_hash = {symbol => [attribute.to_s]}
+          pigeon_list[element].merge!(temp_hash)  
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
