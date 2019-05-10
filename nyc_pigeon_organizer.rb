7e3f58943d832a0fc6457ea21dee56d6e0024ca5
Jess Rezac <jess.rezac@gmail.com>
2019-05-10 16:43:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,13 @@
 require "pry"
 
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
-  data.each do |trait, variants|
+  data.each_with_object({}) do |(trait, variants), result|
     variants.each do |variant, birds|
       birds.each do |bird|
-        if pigeon_list.include?(bird) && pigeon_list[bird].include?(trait)
-          pigeon_list[bird][trait] << variant.to_s
-        elsif pigeon_list.include?(bird)
-          pigeon_list[bird][trait] = []
-          pigeon_list[bird][trait] << variant.to_s
-        else
-          pigeon_list[bird] = {}
-          pigeon_list[bird][trait] = []
-          pigeon_list[bird][trait] << variant.to_s
-        end
+        result[bird] ||= {}
+        result[bird][trait] ||= []
+        result[bird][trait] << variant.to_s
       end
     end
   end
-
-  pigeon_list
 end
